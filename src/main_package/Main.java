e70b01243195cd78f7898c89372d334a6505cb88
YoungPotato <danilov.grischa2015@yandex.ru>
2019-10-08 00:11:49 +0500

    second commit

---
+++ src/main_package/Main.java
@@ -0,0 +1,13 @@
+package main_package;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    IO io = new IO();
+	    io.printStart();
+	    Logic logic = new Logic();
+	    logic.start();
+        }
+}
